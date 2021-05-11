bba6730908341561ced87f4f1fbae9980f3f9ec1
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-11 14:46:49 +0200

    server: setup basic server using Types from express

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {});
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>Hello there</div>
+    `);
+});
+
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
