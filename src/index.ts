0268dccbac145e0e8fc1e62d9bb1b3c153848a63
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-11 15:30:00 +0200

    login form get and post

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(port, () => {
