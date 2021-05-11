034623abe4be4d5af3e7de44591b0386657bda3e
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-11 14:52:51 +0200

    routes: refactor to components in seperate folder

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(`
-    <div>Hello there</div>
-    `);
-});
+app.use(router);
 
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
